3be1c9bad7fc3a8c2eab45ea8cb1cfe51c33aac6
JhixzK <estoquia.jhixzkyle@ue.edu.ph>
2026-02-10 23:10:41 +0800

    Revert "Update applications.php"
    This reverts commit 65b26c957cbeefa1bacfe9bc5d55cc756e816ad8.

--- Barangay219/public/applications.php
+++ Barangay219/public/applications.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../includes/auth-check.php';
 
 requireLogin();
-requireModuleAccess('applications');
+requireAnyRole([ROLE_BARANGAY_CAPTAIN, ROLE_SECRETARY]);
 
 $page_title = 'Certificate Applications';
 include __DIR__ . '/../includes/header.php';
