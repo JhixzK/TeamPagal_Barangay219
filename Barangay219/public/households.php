dc49304295429737708255830723fd3920d35346
JhixzK <estoquia.jhixzkyle@ue.edu.ph>
2026-02-11 00:05:44 +0800

    Update households.php

--- Barangay219/public/households.php
+++ Barangay219/public/households.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../includes/header.php';
 require_once __DIR__ . '/../includes/auth-check.php';
 
 requireLogin();
-requireAnyRole([ROLE_BARANGAY_CAPTAIN, ROLE_SECRETARY]);
+requireModuleAccess('households');
 
 include __DIR__ . '/../includes/sidebar.php';
 ?> 
