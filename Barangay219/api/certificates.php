2e924104fe8d81de86b570636a1b0e90e53ebc49
JhixzK <estoquia.jhixzkyle@ue.edu.ph>
2026-02-11 00:05:18 +0800

    Update certificates.php

--- Barangay219/api/certificates.php
+++ Barangay219/api/certificates.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../includes/auth-check.php';
 
 requireLogin();
-requireAnyRole([ROLE_BARANGAY_CAPTAIN, ROLE_SECRETARY, ROLE_TREASURER]);
+requireModuleAccess('certificates');
 
 $action = $_GET['action'] ?? $_POST['action'] ?? '';
 
