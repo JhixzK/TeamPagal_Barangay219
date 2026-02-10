dc6af1f1a2cfe00d28ea6b492426c182bbbb5c87
JhixzK <estoquia.jhixzkyle@ue.edu.ph>
2026-02-10 21:54:29 +0800

    Update header.php

--- Barangay219/includes/header.php
+++ Barangay219/includes/header.php
@@ -46,26 +46,7 @@ $userInfo = getUserInfo();
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown">
-                            <i class="bi bi-person-circle"></i> 
-                            <?php echo htmlspecialchars($userInfo['username'] ?? 'User'); ?>
-                            <span class="badge bg-light text-dark ms-1"><?php echo ucfirst(str_replace('_', ' ', getCurrentUserRole())); ?></span>
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="<?php echo BASE_URL; ?>profile.php">
-                                <i class="bi bi-person"></i> Profile
-                            </a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="#" onclick="logout()">
-                                <i class="bi bi-box-arrow-right"></i> Logout
-                            </a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
+            <div class="collapse navbar-collapse" id="navbarNav"></div>
         </div>
     </nav>
     <?php endif; ?>
