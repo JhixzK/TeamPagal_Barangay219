1eef330287af03c452064278085c9dd09898a757
JhixzK <estoquia.jhixzkyle@ue.edu.ph>
2026-02-10 23:08:55 +0800

    Revert "Update dashboard.php"
    This reverts commit 38b04fd645eaa413ce97c177d919acac1bb012f8.

--- Barangay219/public/dashboard.php
+++ Barangay219/public/dashboard.php
@@ -3,7 +3,6 @@ define('ACCESS_ALLOWED', true);
 require_once __DIR__ . '/../includes/auth-check.php';
 
 requireLogin();
-requireModuleAccess('dashboard');
 
 $page_title = 'Dashboard';
 include __DIR__ . '/../includes/header.php';
