410267d9947a143428e9db8e1516d678aaf11b07
JhixzK <estoquia.jhixzkyle@ue.edu.ph>
2026-02-10 23:09:58 +0800

    Revert "Update households.php"
    This reverts commit 1a208aeec808f2eb32fe5a0bd73bec0a96f8a4ed.

--- Barangay219/public/households.php
+++ Barangay219/public/households.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../includes/header.php';
 require_once __DIR__ . '/../includes/auth-check.php';
 
 requireLogin();
-requireModuleAccess('households');
+requireAnyRole([ROLE_BARANGAY_CAPTAIN, ROLE_SECRETARY]);
 
 include __DIR__ . '/../includes/sidebar.php';
 ?> 
