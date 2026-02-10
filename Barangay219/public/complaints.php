ec646171d5b5f1108c3e4f3e586a743d49906914
JhixzK <estoquia.jhixzkyle@ue.edu.ph>
2026-02-10 22:43:21 +0800

    Update complaints.php

--- Barangay219/public/complaints.php
+++ Barangay219/public/complaints.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../includes/auth-check.php';
 
 requireLogin();
-requireAnyRole([ROLE_BARANGAY_CAPTAIN, ROLE_SECRETARY, ROLE_KAGAWA]);
+requireModuleAccess('complaints');
 
 $page_title = 'Complaints';
 include __DIR__ . '/../includes/header.php';
