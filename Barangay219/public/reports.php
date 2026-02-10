4472505ccda8169e9af291e288f79a00d0a318c0
JhixzK <estoquia.jhixzkyle@ue.edu.ph>
2026-02-10 23:09:51 +0800

    Revert "Update reports.php"
    This reverts commit b573bdccf61226c1a742aee5e773923ceb65c207.

--- Barangay219/public/reports.php
+++ Barangay219/public/reports.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../includes/auth-check.php';
 
 requireLogin();
-requireModuleAccess('reports');
+requireAnyRole([ROLE_BARANGAY_CAPTAIN, ROLE_SECRETARY, ROLE_TREASURER]);
 
 $page_title = 'Reports';
 include __DIR__ . '/../includes/header.php';
