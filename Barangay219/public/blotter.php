c7633c05e9490d4b7d0c56a8c0fa197a3a6a500b
JhixzK <estoquia.jhixzkyle@ue.edu.ph>
2026-02-10 23:10:26 +0800

    Revert "Update blotter.php"
    This reverts commit 45adb5e081b6890b505039c716a5fa498f1b5234.

--- Barangay219/public/blotter.php
+++ Barangay219/public/blotter.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../includes/header.php';
 require_once __DIR__ . '/../includes/auth-check.php';
 
 requireLogin();
-requireModuleAccess('blotters');
+requireAnyRole([ROLE_BARANGAY_CAPTAIN, ROLE_KAGAWA]);
 
 include __DIR__ . '/../includes/sidebar.php';
 ?>
