52c7829ae0056f66c9ef467d0ae2f33fcfdf0787
JhixzK <estoquia.jhixzkyle@ue.edu.ph>
2026-02-12 23:31:49 +0800

    Update header.php

--- Barangay219/includes/header.php
+++ Barangay219/includes/header.php
@@ -27,7 +27,7 @@ $userInfo = getUserInfo();
     <!-- Bootstrap Icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
     <!-- Custom CSS -->
-    <link href="<?php echo ASSETS_URL; ?>style.css" rel="stylesheet">
+    <link href="<?php echo ASSETS_URL; ?>style.css?v=<?php echo time(); ?>" rel="stylesheet">
     
     <!-- Define API URL for JavaScript (available on all pages) -->
     <script>
