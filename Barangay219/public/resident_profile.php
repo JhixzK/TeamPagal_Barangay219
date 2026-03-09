69c0877d9afbc080eb63ebc4564964785fc506a1
Kenneth <erfekenneth9@gmail.com>
2026-03-10 00:49:54 +0800

    Update resident_profile.php

--- Barangay219/public/resident_profile.php
+++ Barangay219/public/resident_profile.php
@@ -269,7 +269,7 @@ $verificationBadge = match($displayData['verification_status']) {
 
       <div class="nav-group">
         <p class="group-title label">COMMUNITY</p>
-        <a class="nav-item" href="#">
+        <a class="nav-item" href="<?php echo BASE_URL; ?>resident_announcements.php">
           <i class="fa-regular fa-newspaper"></i>
           <span class="label">Announcements</span>
         </a>
