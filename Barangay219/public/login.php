737a73f385eb3a9c50b20b1eea4aae5456f4a76a
JhixzK <estoquia.jhixzkyle@ue.edu.ph>
2026-03-13 18:06:09 +0800

    Update login.php

--- Barangay219/public/login.php
+++ Barangay219/public/login.php
@@ -49,6 +49,8 @@ if (isLoggedIn()) {
             background: url('<?php echo ASSETS_URL; ?>img/crop219logo.png') no-repeat 95% center;
             background-size: calc(900px * var(--bg-zoom-inverse, 1));
             opacity: 0.90;
+            filter: blur(6px);
+            transform: scale(1.03);
             pointer-events: none;
         }
 
