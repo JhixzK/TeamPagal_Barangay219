000cda8482e8fbbd50ca61d386e17c913aeee66a
JhixzK <estoquia.jhixzkyle@ue.edu.ph>
2026-03-19 21:58:45 +0800

    Update residents.php

--- Barangay219/public/residents.php
+++ Barangay219/public/residents.php
@@ -137,7 +137,7 @@ include __DIR__ . '/../includes/sidebar.php';
 
 <!-- Filter Modal -->
 <div class="modal fade" id="filterModal" tabindex="-1">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Filter Residents</h5>
@@ -184,7 +184,7 @@ include __DIR__ . '/../includes/sidebar.php';
 
 <!-- Resident Modal -->
 <div class="modal fade" id="residentModal" tabindex="-1">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="residentModalTitle">Edit Resident</h5>
@@ -307,7 +307,7 @@ include __DIR__ . '/../includes/sidebar.php';
 
 <!-- View Resident Modal -->
 <div class="modal fade" id="viewResidentModal" tabindex="-1">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Resident Details</h5>
