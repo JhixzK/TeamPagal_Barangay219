f64ede9fae462f5fa5b76481826774b37b4ccbc0
JhixzK <estoquia.jhixzkyle@ue.edu.ph>
2026-03-22 03:16:19 +0800

    Update blotter.php

--- Barangay219/public/blotter.php
+++ Barangay219/public/blotter.php
@@ -62,7 +62,7 @@ include __DIR__ . '/../includes/sidebar.php';
                         <th class="text-center">Incident Location</th>
                         <th class="text-center">Date</th>
                         <th class="text-center">Status</th>
-                        <th class="text-center blotter-actions-col">Actions</th>
+                        <th class="text-center blotter-actions-col actions-col-compact">Actions</th>
                     </tr>
                 </thead>
                 <tbody id="blotterTableBody"><tr><td colspan="8" class="text-center">Loading...</td></tr></tbody>
@@ -539,10 +539,6 @@ include __DIR__ . '/../includes/sidebar.php';
         background: #fff1f3;
         border-color: #f6ccd3;
         color: #9f2f3e;
-    }
-
-    .blotter-page .blotter-actions-col {
-        min-width: 140px;
     }
 
     @media (max-width: 768px) {
