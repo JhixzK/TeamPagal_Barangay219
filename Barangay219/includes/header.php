e413776c525cea67b5b31361a19343edfa5f403d
JhixzK <estoquia.jhixzkyle@ue.edu.ph>
2026-03-22 14:47:51 +0800

    Update header.php

--- Barangay219/includes/header.php
+++ Barangay219/includes/header.php
@@ -28,6 +28,9 @@ $userInfo = getUserInfo();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
     <!-- Custom CSS -->
     <link href="<?php echo ASSETS_URL; ?>style.css?v=<?php echo time(); ?>" rel="stylesheet">
+    <?php if (isResidentView()): ?>
+    <link href="<?php echo ASSETS_URL; ?>css/resident-official-theme.css?v=<?php echo time(); ?>" rel="stylesheet">
+    <?php endif; ?>
     
     <!-- Define API URL for JavaScript (available on all pages) -->
     <script>
